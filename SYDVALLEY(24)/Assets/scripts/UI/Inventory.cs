d2d5e2b43bbc8ba2579556b193aea2e8e2cc95e4
Brayden Smith <bsmit104@ucsc.edu>
2024-08-04 18:42:44 -0700

    fixed drag

--- SYDVALLEY(24)/Assets/scripts/UI/Inventory.cs
+++ SYDVALLEY(24)/Assets/scripts/UI/Inventory.cs
@@ -144,6 +144,7 @@ public class Inventory : MonoBehaviour
     }
 }
 
+
 // using System;
 // using System.Collections.Generic;
 // using UnityEngine;
@@ -228,8 +229,7 @@ public class Inventory : MonoBehaviour
 //         return false;
 //     }
 
-//     [Obsolete]
-//     public bool RemoveItem(Item itemToRemove, int quantity = 1)
+//     public void RemoveItem(Item itemToRemove, int quantity = 1)
 //     {
 //         Debug.Log($"Trying to remove {quantity} of {itemToRemove.itemName}");
 //         for (int i = items.Count - 1; i >= 0; i--)
@@ -253,12 +253,17 @@ public class Inventory : MonoBehaviour
 //                 if (quantity == 0)
 //                 {
 //                     OnInventoryChanged?.Invoke();
-//                     return true;
+//                     // Deselect the item if it's the selected item being removed
+//                     if (selectedItem == stack.item)
+//                     {
+//                         selectedItem = null;
+//                         OnSelectedItemChanged?.Invoke(null);
+//                     }
+//                     return;
 //                 }
 //             }
 //         }
 //         OnInventoryChanged?.Invoke();
-//         return quantity == 0;
 //     }
 
 //     public void SelectItem(int index)
@@ -276,7 +281,6 @@ public class Inventory : MonoBehaviour
 //         return selectedItem;
 //     }
 
-//     [Obsolete]
 //     public void GiveSelectedItemToNPC(NPCInteraction npcInteraction)
 //     {
 //         if (selectedItem != null && npcInteraction != null)
@@ -284,419 +288,5 @@ public class Inventory : MonoBehaviour
 //             npcInteraction.GiveGift(selectedItem);
 //             RemoveItem(selectedItem, 1); // Remove one instance of the item from inventory
 //         }
-//     }
-// }
-
-
-
-////////////////good with updated trash and selected item////////////////////
-// using System;
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class Inventory : MonoBehaviour
-// {
-//     [SerializeField]
-//     public List<ItemStack> items = new List<ItemStack>();
-
-//     [System.Serializable]
-//     public class ItemStack
-//     {
-//         public Item item;
-//         public int stackSize;
-//     }
-
-//     public Item someItemReference;
-
-//     // Event to notify when the inventory changes
-//     public event Action OnInventoryChanged;
-
-//     // Event to notify when the selected item changes
-//     public event Action<Item> OnSelectedItemChanged;
-
-//     private Item selectedItem;
-
-//     void Start()
-//     {
-//         if (someItemReference != null)
-//         {
-//             items.Add(new ItemStack { item = someItemReference, stackSize = 1 });
-//             Debug.Log("Added test item to inventory: " + someItemReference.itemName);
-//             OnInventoryChanged?.Invoke();
-//         }
-//         else
-//         {
-//             Debug.LogWarning("someItemReference is not assigned.");
-//         }
-//     }
-
-//     void Update()
-//     {
-//         HandleNumberKeyInput();
-//     }
-
-//     private void HandleNumberKeyInput()
-//     {
-//         for (int i = 0; i < 9; i++)
-//         {
-//             if (Input.GetKeyDown(KeyCode.Alpha1 + i))
-//             {
-//                 SelectItem(i);
-//             }
-//         }
-//     }
-
-//     public bool IsFull()
-//     {
-//         return items.Count >= 9; // Assuming 9 is the maximum number of different items the inventory can hold
-//     }
-
-//     public bool AddItem(Item itemToAdd, int quantity = 1)
-//     {
-//         foreach (var itemStack in items)
-//         {
-//             if (itemStack.item.itemName == itemToAdd.itemName && itemStack.stackSize < itemStack.item.maxStack)
-//             {
-//                 int availableSpace = itemStack.item.maxStack - itemStack.stackSize;
-//                 int toAdd = Mathf.Min(quantity, availableSpace);
-//                 itemStack.stackSize += toAdd;
-//                 quantity -= toAdd;
-//                 if (quantity == 0)
-//                 {
-//                     OnInventoryChanged?.Invoke();
-//                     return true;
-//                 }
-//             }
-//         }
-//         if (quantity > 0 && !IsFull())
-//         {
-//             items.Add(new ItemStack { item = itemToAdd, stackSize = quantity });
-//             OnInventoryChanged?.Invoke();
-//             return true;
-//         }
-//         return false;
-//     }
-
-//     // public bool RemoveItem(Item itemToRemove, int quantity = 1)
-//     // {
-//     //     Debug.Log($"Trying to remove {quantity} of {itemToRemove.itemName}");
-//     //     for (int i = items.Count - 1; i >= 0; i--)
-//     //     {
-//     //         ItemStack stack = items[i];
-//     //         if (stack.item.itemName == itemToRemove.itemName)
-//     //         {
-//     //             if (quantity >= stack.stackSize)
-//     //             {
-//     //                 quantity -= stack.stackSize;
-//     //                 Debug.Log($"Removing stack {stack.stackSize} of {stack.item.itemName}");
-//     //                 items.RemoveAt(i);
-//     //             }
-//     //             else
-//     //             {
-//     //                 stack.stackSize -= quantity;
-//     //                 Debug.Log($"Reducing stack by {quantity}, new count {stack.stackSize}");
-//     //                 quantity = 0;
-//     //             }
-
-//     //             if (quantity == 0)
-//     //             {
-//     //                 OnInventoryChanged?.Invoke();
-//     //                 return true;
-//     //             }
-//     //         }
-//     //     }
-//     //     OnInventoryChanged?.Invoke();
-//     //     return quantity == 0;
-//     // }
-
-//     [Obsolete]
-//     public bool RemoveItem(Item itemToRemove, int quantity = 1)
-// {
-//     Debug.Log($"Trying to remove {quantity} of {itemToRemove.itemName}");
-//     for (int i = items.Count - 1; i >= 0; i--)
-//     {
-//         ItemStack stack = items[i];
-//         if (stack.item.itemName == itemToRemove.itemName)
-//         {
-//             if (quantity >= stack.stackSize)
-//             {
-//                 quantity -= stack.stackSize;
-//                 Debug.Log($"Removing stack {stack.stackSize} of {stack.item.itemName}");
-//                 items.RemoveAt(i);
-//             }
-//             else
-//             {
-//                 stack.stackSize -= quantity;
-//                 Debug.Log($"Reducing stack by {quantity}, new count {stack.stackSize}");
-//                 quantity = 0;
-//             }
-
-//             if (quantity == 0)
-//             {
-//                 OnInventoryChanged?.Invoke();
-
-//                 // Get reference to the InventoryUI script
-//                 InventoryUI inventoryUI = FindObjectOfType<InventoryUI>();
-//                 if (inventoryUI != null)
-//                 {
-//                     // Update the selected item display if the removed item was the selected item
-//                     if (selectedItem == itemToRemove)
-//                     {
-//                         selectedItem = null; // Deselect the item
-//                         OnSelectedItemChanged?.Invoke(null); // Notify that no item is selected
-//                         inventoryUI.UpdateSelectedItemDisplay(null); // Update the display
-//                     }
-//                 }
-//                 return true;
-//             }
-//         }
-//     }
-//     OnInventoryChanged?.Invoke();
-//     return quantity == 0;
-// }
-
-//     public void SelectItem(int index)
-//     {
-//         if (index >= 0 && index < items.Count)
-//         {
-//             selectedItem = items[index].item;
-//             OnSelectedItemChanged?.Invoke(selectedItem);
-//             Debug.Log("Selected item: " + selectedItem.itemName);
-//         }
-//     }
-
-//     public Item GetSelectedItem()
-//     {
-//         return selectedItem;
-//     }
-// }
-
-///////////////////////gooood////////////////////////
-// public class Inventory : MonoBehaviour
-// {
-//     [SerializeField]
-//     public List<ItemStack> items = new List<ItemStack>();
-
-//     [System.Serializable]
-//     public class ItemStack
-//     {
-//         public Item item;
-//         public int stackSize;
-//     }
-
-//     public Item someItemReference;
-
-//     // Event to notify when the inventory changes
-//     public event Action OnInventoryChanged;
-
-//     void Start()
-//     {
-//         if (someItemReference != null)
-//         {
-//             items.Add(new ItemStack { item = someItemReference, stackSize = 1 });
-//             Debug.Log("Added test item to inventory: " + someItemReference.itemName);
-//             OnInventoryChanged?.Invoke();
-//         }
-//         else
-//         {
-//             Debug.LogWarning("someItemReference is not assigned.");
-//         }
-//     }
-
-//     public bool IsFull()
-//     {
-//         return items.Count >= 9; // Assuming 9 is the maximum number of different items the inventory can hold
-//     }
-//     public bool AddItem(Item itemToAdd, int quantity = 1)
-//     {
-//         foreach (var itemStack in items)
-//         {
-//             if (itemStack.item.itemName == itemToAdd.itemName && itemStack.stackSize < itemStack.item.maxStack)
-//             {
-//                 int availableSpace = itemStack.item.maxStack - itemStack.stackSize;
-//                 int toAdd = Mathf.Min(quantity, availableSpace);
-//                 itemStack.stackSize += toAdd;
-//                 quantity -= toAdd;
-//                 if (quantity == 0)
-//                 {
-//                     OnInventoryChanged?.Invoke();
-//                     return true;
-//                 }
-//             }
-//         }
-//         if (quantity > 0 && !IsFull())
-//         {
-//             items.Add(new ItemStack { item = itemToAdd, stackSize = quantity });
-//             OnInventoryChanged?.Invoke();
-//             return true;
-//         }
-//         return false;
-//     }
-
-//     public bool RemoveItem(Item itemToRemove, int quantity = 1)
-//     {
-//         Debug.Log($"Trying to remove {quantity} of {itemToRemove.itemName}");
-//         for (int i = items.Count - 1; i >= 0; i--)
-//         {
-//             ItemStack stack = items[i];
-//             if (stack.item.itemName == itemToRemove.itemName)
-//             {
-//                 if (quantity >= stack.stackSize)
-//                 {
-//                     quantity -= stack.stackSize;
-//                     Debug.Log($"Removing stack {stack.stackSize} of {stack.item.itemName}");
-//                     items.RemoveAt(i);
-//                 }
-//                 else
-//                 {
-//                     stack.stackSize -= quantity;
-//                     Debug.Log($"Reducing stack by {quantity}, new count {stack.stackSize}");
-//                     quantity = 0;
-//                 }
-
-//                 if (quantity == 0)
-//                 {
-//                     OnInventoryChanged?.Invoke();
-//                     return true;
-//                 }
-//             }
-//         }
-//         OnInventoryChanged?.Invoke();
-//         return quantity == 0;
-//     }
-// }
-
-////////////////////////////////////////////////////////
-
-
-
-
-
-    // public bool RemoveItem(Item itemToRemove, int quantity = 1)
-    // {
-    //     for (int i = items.Count - 1; i >= 0; i--)
-    //     {
-    //         ItemStack stack = items[i];
-    //         if (stack.item.itemName == itemToRemove.itemName)
-    //         {
-    //             if (quantity >= stack.stackSize)
-    //             {
-    //                 quantity -= stack.stackSize;
-    //                 items.RemoveAt(i);
-    //             }
-    //             else
-    //             {
-    //                 stack.stackSize -= quantity;
-    //                 OnInventoryChanged?.Invoke();
-    //                 return true;
-    //             }
-    //         }
-    //     }
-    //     OnInventoryChanged?.Invoke();
-    //     return quantity == 0;
-    // }
-
-
-    // public void AddItem(Item itemToAdd, int quantity = 1)
-    // {
-    //     foreach (var itemStack in items)
-    //     {
-    //         if (itemStack.item.itemName == itemToAdd.itemName && itemStack.stackSize < itemStack.item.maxStack)
-    //         {
-    //             int availableSpace = itemStack.item.maxStack - itemStack.stackSize;
-    //             int toAdd = Mathf.Min(quantity, availableSpace);
-    //             itemStack.stackSize += toAdd;
-    //             quantity -= toAdd;
-    //             if (quantity == 0)
-    //             {
-    //                 OnInventoryChanged?.Invoke();
-    //                 return;
-    //             }
-    //         }
-    //     }
-    //     if (quantity > 0)
-    //     {
-    //         items.Add(new ItemStack { item = itemToAdd, stackSize = quantity });
-    //         OnInventoryChanged?.Invoke();
-    //     }
-    // }
-
-
-
-
-
-// using System.Collections.Generic;
-// using UnityEngine;
-
-// public class Inventory : MonoBehaviour
-// {
-//     [SerializeField]
-//     public List<ItemStack> items = new List<ItemStack>();
-
-//     [System.Serializable]
-//     public class ItemStack
-//     {
-//         public Item item;
-//         public int stackSize;
-//     }
-
-//     // Reference to an item for testing purposes
-//     public Item someItemReference;
-
-//     void Start()
-//     {
-//         // Initial setup if necessary
-//         if (someItemReference != null)
-//         {
-//             items.Add(new ItemStack { item = someItemReference, stackSize = 1 });
-//             Debug.Log("Added test item to inventory: " + someItemReference.itemName);
-//         }
-//         else
-//         {
-//             Debug.LogWarning("someItemReference is not assigned.");
-//         }
-//     }
-
-//     // Additional methods will be added here
-
-//     public void AddItem(Item itemToAdd, int quantity = 1)
-//     {
-//         foreach (var itemStack in items)
-//         {
-//             if (itemStack.item.itemName == itemToAdd.itemName && itemStack.stackSize < itemStack.item.maxStack)
-//             {
-//                 int availableSpace = itemStack.item.maxStack - itemStack.stackSize;
-//                 int toAdd = Mathf.Min(quantity, availableSpace);
-//                 itemStack.stackSize += toAdd;
-//                 quantity -= toAdd;
-//                 if (quantity == 0) return;
-//             }
-//         }
-//         if (quantity > 0)
-//         {
-//             items.Add(new ItemStack { item = itemToAdd, stackSize = quantity });
-//         }
-//     }
-
-//     public bool RemoveItem(Item itemToRemove, int quantity = 1)
-//     {
-//         for (int i = items.Count - 1; i >= 0; i--)
-//         {
-//             ItemStack stack = items[i];
-//             if (stack.item.itemName == itemToRemove.itemName)
-//             {
-//                 if (quantity >= stack.stackSize)
-//                 {
-//                     quantity -= stack.stackSize;
-//                     items.RemoveAt(i);
-//                 }
-//                 else
-//                 {
-//                     stack.stackSize -= quantity;
-//                     return true;
-//                 }
-//             }
-//         }
-//         return quantity == 0;
 //     }
 // }
--- SYDVALLEY(24)/Assets/scripts/UI/InventoryUI.cs
+++ SYDVALLEY(24)/Assets/scripts/UI/InventoryUI.cs
@@ -91,10 +91,16 @@ public class InventoryUI : MonoBehaviour
         GameObject itemObject = Instantiate(draggedItemPrefab);
         Image itemImage = itemObject.GetComponent<Image>();
         itemImage.sprite = inventory.items[index].item.itemIcon;
-        itemImage.SetNativeSize();
-        Canvas canvas = FindFirstObjectByType<Canvas>();
+
+        // Set size using RectTransform
+        RectTransform rectTransform = itemObject.GetComponent<RectTransform>();
+        rectTransform.sizeDelta = new Vector2(50, 50); // Set desired size here
+        rectTransform.anchoredPosition = Vector2.zero; // Center the item initially
+
+        // Set parent to Canvas
+        Canvas canvas = FindObjectOfType<Canvas>();
         itemObject.transform.SetParent(canvas.transform, false);
-        itemObject.transform.SetAsLastSibling();
+
         return itemObject;
     }
 
@@ -103,9 +109,18 @@ public class InventoryUI : MonoBehaviour
         if (draggedItem != null)
         {
             PointerEventData pointerData = (PointerEventData)data;
-            Vector3 newPosition = Camera.main.ScreenToWorldPoint(pointerData.position);
-            newPosition.z = 0; // Assuming your dragged items should be on the same z-plane
-            draggedItem.transform.position = newPosition;
+
+            // Get the Canvas
+            Canvas canvas = FindObjectOfType<Canvas>();
+            RectTransform canvasRectTransform = canvas.GetComponent<RectTransform>();
+
+            // Convert screen point to local point
+            Vector2 localPoint;
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(canvasRectTransform, pointerData.position, canvas.worldCamera, out localPoint);
+
+            // Set the position of the dragged item
+            RectTransform draggedItemRectTransform = draggedItem.GetComponent<RectTransform>();
+            draggedItemRectTransform.anchoredPosition = localPoint;
         }
     }
 
@@ -220,4 +235,233 @@ public class InventoryUI : MonoBehaviour
         }
     }
 }
+
+
+
+
+
+
+
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.UI;
+// using UnityEngine.EventSystems;
+// using TMPro;
+
+// public class InventoryUI : MonoBehaviour
+// {
+//     [SerializeField]
+//     private Inventory inventory;
+//     public Transform slotPanel;
+//     public Image trashCanImage; // Assign your trash can image in the editor
+//     public Transform playerHeadTransform; // Assign the player's head transform in the editor
+//     public GameObject selectedItemDisplayPrefab; // Prefab for displaying selected item above the player's head
+//     public GameObject draggedItemPrefab; // Prefab for displaying dragged item
+
+//     private List<GameObject> slots = new List<GameObject>();
+//     private GameObject selectedItemDisplay;
+//     private GameObject draggedItem;
+//     private int draggedItemIndex = -1;
+
+//     [System.Obsolete]
+//     void Start()
+//     {
+//         Debug.Log("Initializing inventory UI...");
+
+//         if (inventory == null)
+//         {
+//             Debug.LogError("Inventory is not assigned!");
+//             return;
+//         }
+
+//         inventory.OnInventoryChanged += UpdateInventoryUI;
+//         inventory.OnSelectedItemChanged += UpdateSelectedItemDisplay;
+
+//         InitializeSlots();
+//         UpdateInventoryUI();
+//     }
+
+//     [System.Obsolete]
+//     void InitializeSlots()
+//     {
+//         Debug.Log("Initializing slots...");
+//         for (int i = 0; i < slotPanel.childCount; i++)
+//         {
+//             GameObject slot = slotPanel.GetChild(i).gameObject;
+//             slots.Add(slot);
+
+//             if (slot.GetComponent<CanvasGroup>() == null)
+//             {
+//                 slot.AddComponent<CanvasGroup>();
+//             }
+
+//             EventTrigger trigger = slot.AddComponent<EventTrigger>();
+//             AddEventTrigger(trigger, EventTriggerType.PointerClick, OnSlotClick);
+//             AddEventTrigger(trigger, EventTriggerType.BeginDrag, BeginDrag);
+//             AddEventTrigger(trigger, EventTriggerType.Drag, Drag);
+//             AddEventTrigger(trigger, EventTriggerType.EndDrag, EndDrag);
+//         }
+//         Debug.Log("Slots initialized. Total slots: " + slots.Count);
+//     }
+
+//     void AddEventTrigger(EventTrigger trigger, EventTriggerType eventType, UnityEngine.Events.UnityAction<BaseEventData> action)
+//     {
+//         EventTrigger.Entry entry = new EventTrigger.Entry { eventID = eventType };
+//         entry.callback.AddListener(action);
+//         trigger.triggers.Add(entry);
+//     }
+
+//     void OnSlotClick(BaseEventData data)
+//     {
+//         PointerEventData pointerData = (PointerEventData)data;
+//         int slotIndex = GetSlotIndex(pointerData.pointerPress);
+//         inventory.SelectItem(slotIndex);
+//     }
+
+//     public void BeginDrag(BaseEventData data)
+//     {
+//         PointerEventData pointerData = (PointerEventData)data;
+//         draggedItemIndex = GetSlotIndex(pointerData.pointerPress);
+//         if (draggedItemIndex >= 0 && draggedItemIndex < inventory.items.Count)
+//         {
+//             draggedItem = CreateDraggedItem(draggedItemIndex);
+//             slots[draggedItemIndex].GetComponent<CanvasGroup>().blocksRaycasts = false;
+//         }
+//     }
+
+//     GameObject CreateDraggedItem(int index)
+//     {
+//         Debug.Log("Creating dragged item...");
+//         GameObject itemObject = Instantiate(draggedItemPrefab);
+//         Image itemImage = itemObject.GetComponent<Image>();
+//         itemImage.sprite = inventory.items[index].item.itemIcon;
+//         itemImage.SetNativeSize();
+//         Canvas canvas = FindFirstObjectByType<Canvas>();
+//         itemObject.transform.SetParent(canvas.transform, false);
+//         itemObject.transform.SetAsLastSibling();
+//         return itemObject;
+//     }
+
+//     public void Drag(BaseEventData data)
+//     {
+//         if (draggedItem != null)
+//         {
+//             PointerEventData pointerData = (PointerEventData)data;
+//             Vector3 newPosition = Camera.main.ScreenToWorldPoint(pointerData.position);
+//             newPosition.z = 0; // Assuming your dragged items should be on the same z-plane
+//             draggedItem.transform.position = newPosition;
+//         }
+//     }
+
+//     [System.Obsolete]
+//     public void EndDrag(BaseEventData data)
+//     {
+//         if (draggedItem != null)
+//         {
+//             slots[draggedItemIndex].GetComponent<CanvasGroup>().blocksRaycasts = true;
+
+//             if (IsPointerOverUIObject(trashCanImage.gameObject))
+//             {
+//                 Debug.Log("Item dropped on trash can.");
+//                 if (draggedItemIndex >= 0 && draggedItemIndex < inventory.items.Count)
+//                 {
+//                     inventory.RemoveItem(inventory.items[draggedItemIndex].item, 1);  // Always remove one item
+//                     inventory.SelectItem(-1); // Deselect the item after removing it
+//                 }
+//             }
+
+//             Destroy(draggedItem);
+//             draggedItem = null;
+//             draggedItemIndex = -1;
+
+//             UpdateInventoryUI();
+//         }
+//     }
+
+//     int GetSlotIndex(GameObject slot)
+//     {
+//         return slots.IndexOf(slot);
+//     }
+
+//     bool IsPointerOverUIObject(GameObject uiObject)
+//     {
+//         PointerEventData pointerEventData = new PointerEventData(EventSystem.current) { position = Input.mousePosition };
+//         List<RaycastResult> results = new List<RaycastResult>();
+//         EventSystem.current.RaycastAll(pointerEventData, results);
+//         return results.Exists(result => result.gameObject == uiObject);
+//     }
+
+//     public void UpdateInventoryUI()
+//     {
+//         Debug.Log("Updating inventory UI...");
+//         for (int i = 0; i < slots.Count; i++)
+//         {
+//             Transform iconTransform = slots[i].transform.Find("Icon");
+//             Transform countTransform = slots[i].transform.Find("Count");
+
+//             Image iconImage = iconTransform?.GetComponent<Image>();
+//             TMP_Text countText = countTransform?.GetComponent<TMP_Text>();
+
+//             if (i < inventory.items.Count)
+//             {
+//                 if (iconImage != null)
+//                 {
+//                     iconImage.sprite = inventory.items[i].item.itemIcon;
+//                     iconImage.color = Color.white;
+//                 }
+//                 if (countText != null)
+//                 {
+//                     countText.text = inventory.items[i].stackSize.ToString();
+//                 }
+//             }
+//             else
+//             {
+//                 if (iconImage != null) iconImage.sprite = null;
+//                 if (countText != null) countText.text = "";
+//             }
+//         }
+//     }
+
+//     public void UpdateSelectedItemDisplay(Item selectedItem)
+//     {
+//         // Destroy the previous selectedItemDisplay if it exists
+//         if (selectedItemDisplay != null)
+//         {
+//             Destroy(selectedItemDisplay);
+//             selectedItemDisplay = null;
+//         }
+
+//         // Check if a new item is selected
+//         if (selectedItem != null)
+//         {
+//             // Instantiate a new selectedItemDisplay
+//             selectedItemDisplay = Instantiate(selectedItemDisplayPrefab);
+//             Image itemImage = selectedItemDisplay.GetComponent<Image>();
+//             if (itemImage != null)
+//             {
+//                 itemImage.sprite = selectedItem.itemIcon;
+//             }
+
+//             // Set the parent to the player's head transform
+//             selectedItemDisplay.transform.SetParent(playerHeadTransform);
+
+//             // Set position relative to the player's head
+//             selectedItemDisplay.transform.localPosition = new Vector3(0, 2, 0); // Adjust position above the player's head
+
+//             // Ensure the item is positioned in world space
+//             selectedItemDisplay.transform.localPosition = Vector3.zero; // Reset local position
+//             selectedItemDisplay.transform.localRotation = Quaternion.identity; // Reset local rotation
+//             selectedItemDisplay.transform.localScale = Vector3.one; // Reset local scale
+//         }
+//     }
+
+//     private void OnDestroy()
+//     {
+//         if (inventory != null)
+//         {
+//             inventory.OnInventoryChanged -= UpdateInventoryUI;
+//             inventory.OnSelectedItemChanged -= UpdateSelectedItemDisplay;
+//         }
+//     }
+// }
 
